f693eb721b78d7e8e8ecdddd0d01bf3805af9da7
Felix <gbemilekeogundipe@gmail.com>
2022-01-16 22:30:00 -0800

    Introduction of swagger Ui

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 
+//security packages
 const helmet = require('helmet')
-// const xss = require('x')
+const xss = require('xss-clean')
 const cors = require('cors')
-const swagger = require('swagger-ui-express')
-const yaml = require('yamljs')
+const rateLimit = require('express-rate-limit')
+
+//Swagger-ui-------
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = load('./swagger.yaml')
+const YAML = require('yamljs')
 
 const app = express()
 
@@ -25,15 +30,22 @@ const connectDB = require('./db/docs')
 app.use(express.json())
 
 //security
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
 app.use(helmet())
-// app.use(xss())
+app.use(xss())
 app.use(cors())
 
 //Routes
 app.get('/', (req, res)=>{
-    res.send("WELCOME FELLAS")
+    res.send('WELCOME FELLAS <h1>Edu Api</h1> <a href="/api-docs"> Documentation </a>')
 });
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/api/v1/auth', userRouter)
 app.use('/api/v1/docs', authMiddleware, docsRouter)
 
